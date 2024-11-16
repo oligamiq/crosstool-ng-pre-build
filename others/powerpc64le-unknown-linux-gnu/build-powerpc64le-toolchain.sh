3cd40d826bf8f9789d1ecc1096f892f3ded96ea7
oligami <s20032@tokyo.kosen-ac.jp>
2024-11-16 23:29:01 +0900

    Add toolchain installation steps and error handling for powerpc64le and sparcv9 actions

--- others/powerpc64le-unknown-linux-gnu/build-powerpc64le-toolchain.sh
+++ others/powerpc64le-unknown-linux-gnu/build-powerpc64le-toolchain.sh
@@ -2,7 +2,22 @@
 
 set -ex
 
-source shared.sh
+hide_output() {
+  set +x
+  on_err="
+echo ERROR: An error was encountered with the build.
+cat /tmp/build.log
+exit 1
+"
+  trap "$on_err" ERR
+  bash -c "while true; do sleep 30; echo \$(date) - building ...; done" &
+  PING_LOOP_PID=$!
+  "$@" &> /tmp/build.log
+  rm /tmp/build.log
+  trap - ERR
+  kill $PING_LOOP_PID
+  set -x
+}
 
 BINUTILS=2.32
 GCC=8.3.0
